f941c3e6f007a7bd1bf90ebb92a01170844516f7
IcySteam <IcySteam@users.noreply.github.com>
2024-05-19 03:48:14 +1000

    Update MPUR.bat

--- MPUR.bat
+++ MPUR.bat
@@ -1,9 +1,9 @@
 node proxy-login-automator.js ^
-	-local_host * ^
+    -local_host * ^
     -local_port 8080 ^
     -remote_host ultra.marsproxies.com ^
     -remote_port 44443 ^
-	-marsproxies_random_session true ^
+    -marsproxies_random_session true ^
     -usr USER -pwd PASSWORD_country-au,nz,sg,hk,mo,tw,kr,jp,us,ca_lifetime-168h
 
 cmd
